088dfbcb93eb1fde1fe627b04bdb9b48d9cf077c
Lars Heller <lhe@cedros.com>
2017-10-11 15:58:17 +0200

    added login page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,18 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
 // Containers
 import Full from '@/containers/Full'
-
 // Views
 import Dashboard from '@/views/Dashboard'
+// Pages
+import Login from '@/views/pages/Login'
+import Page404 from '@/views/pages/Page404'
+import Page500 from '@/views/pages/Page500'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'hash',
   linkActiveClass: 'open active',
-  scrollBehavior: () => ({ y: 0 }),
+  scrollBehavior: () => ({y: 0}),
   routes: [
     {
       path: '/',
@@ -26,6 +28,31 @@ export default new Router({
           component: Dashboard
         }
 
+      ]
+    },
+    {
+      path: '/pages',
+      redirect: '/pages/404',
+      name: 'Pages',
+      component: {
+        render (c) { return c('router-view') }
+      },
+      children: [
+        {
+          path: '404',
+          name: 'Page404',
+          component: Page404
+        },
+        {
+          path: '500',
+          name: 'Page500',
+          component: Page500
+        },
+        {
+          path: 'login',
+          name: 'Login',
+          component: Login
+        }
       ]
     }
   ]
---
+++ client/src/views/pages/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app flex-row align-items-center">
+    <div class="container">
+      <div class="row justify-content-center">
+        <img src="static/img/eve_login.png" v-on:click="redirectToSSO"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+  },
+  methods: {
+    redirectToSSO () {
+      let url = 'https://login.eveonline.com/oauth/authorize'
+      url += '?response_type=code'
+      url += '&redirect_uri=http%3A%2F%2Flocalhost%3A8800%2F%23%2Fpages%2Flogin'
+      url += '&client_id=aa398ed41e1a433cbfd7e31e998b6b0c'
+      url += '&scope=esi-planets.manage_planets.v1'
+      url += '&state=eveinabox'
+      window.location = url
+    }
+  }
+}
+</script>
