48b99aff715332eddfb9e723f4032325bbba5043
rudolfson <rudolfson@gmail.com>
2017-11-12 22:49:40 +0100

    adding character page

--- client/src/_nav.js
+++ client/src/_nav.js
@@ -4,6 +4,16 @@ export default {
       name: 'Dashboard',
       url: '/dashboard',
       icon: 'icon-speedometer'
+    },
+    {
+      name: 'Characters',
+      url: '/characters',
+      icon: 'icon-people'
+    },
+    {
+      name: 'Planets',
+      url: '/planets',
+      icon: 'icon-globe-alt'
     }
   ]
 }
--- rest-api/grails-app/domain/eveinabox/Character.groovy
+++ rest-api/grails-app/domain/eveinabox/Character.groovy
@@ -9,10 +9,8 @@ class Character {
     String refreshToken
     Instant expiresOn
 
-    static belongsTo = [user: ApplicationUser]
     static constraints = {
         eveId nullable: false
         name nullable: false, blank: false, maxSize: 200
-        user nullable: false
     }
 }
